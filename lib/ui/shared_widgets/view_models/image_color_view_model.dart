db7dbaf2285b96448b21bf841fc1b742528e66ac
Martin <martin.seal@spicatech.co.uk>
2023-01-14 01:59:13 +0000

    FIX spelling mistake

--- lib/ui/shared_widgets/view_models/image_color_view_model.dart
+++ lib/ui/shared_widgets/view_models/image_color_view_model.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'dart:isolate';
 import 'dart:typed_data';
-import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,8 +7,6 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_isolate/flutter_isolate.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:rxdart/rxdart.dart';
-
-import '../../../utils/console_output.dart';
 
 @pragma('vm:entry-point')
 void computePalette(List<Object> args) async {
@@ -21,11 +17,13 @@ void computePalette(List<Object> args) async {
     image.image,
     maximumColorCount: 7,
   );
-  final colorLost = palette.colors.map(
+  final colorList = palette.colors
+      .map(
         (e) => e.value,
-  ).toList();
+      )
+      .toList();
   port.send(
-    colorLost,
+    colorList,
   );
 }
 
@@ -33,35 +31,43 @@ const int kPaletteError = 101;
 
 class ImageColorViewModel {
   final colorListStream = BehaviorSubject<List<int>>();
+  FlutterIsolate? isolate;
 
   void updatePalette(
     BuildContext context,
     CachedNetworkImageProvider imageProvider,
   ) async {
-    var file = await DefaultCacheManager().getSingleFile(imageProvider.url);
-    final bytes = await file.readAsBytes();
-    final port = ReceivePort();
-    final isolate = await FlutterIsolate.spawn(
-      computePalette,
-      [
-        bytes,
-        port.sendPort,
-      ],
-    );
-    port.listen(
+    if (!colorListStream.isClosed) {
+      var file = await DefaultCacheManager().getSingleFile(
+        imageProvider.url,
+      );
+      final bytes = await file.readAsBytes();
+      final port = ReceivePort();
+      if (!colorListStream.isClosed) {
+        isolate = await FlutterIsolate.spawn(
+          computePalette,
+          [
+            bytes,
+            port.sendPort,
+          ],
+        );
+        port.listen(
           (message) {
-        if (!colorListStream.isClosed) {
-          colorListStream.add(
-            message,
-          );
-        }
-        isolate.kill();
-        port.close();
-      },
-    );
+            if (!colorListStream.isClosed) {
+              colorListStream.add(
+                message,
+              );
+            }
+            isolate?.kill();
+            port.close();
+          },
+        );
+      }
+    }
   }
 
   void dispose() {
+    isolate?.kill();
     colorListStream.close();
   }
 }
